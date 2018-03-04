287d70baea293c2b1d06ab3109a3503a93707b5e
jiangjj <149521920@qq.com>
2018-03-05 00:42:36 +0800

    Initial commit

---
+++ src/main/java/com/jiangjj/corejava/v1ch03/FirstSample.java
@@ -0,0 +1,12 @@
+package com.jiangjj.corejava.v1ch03;
+
+/**
+ * This is the first sample program in Core Java Chapter3
+ * @version 1.01 2018-03-04
+ * @author jiangjj
+ */
+public class FirstSample {
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello, World!'");
+    }
+}
